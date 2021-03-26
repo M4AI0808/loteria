e1101f32f5950c1685cde97aa2f27eae865916cf
M4AI0808 <kokosik1818bartek@gmail.com>
2021-03-26 14:47:17 +0100

    V1.2

--- malytotek.py
+++ malytotek.py
@@ -4,7 +4,7 @@
 import random
 liczba = random.randint  (1,10)
 odpowiedz = input ('Jaką liczbe od 1-10 mam na myśli?:')
-while int (odpowiedz) > 31:
+while int (odpowiedz) > 31 or odpowiedz < 1:
     print ('ZŁa LICZBA, TYLKO LICZBY OD 1 DO 10. ')
     exit()
 if liczba==int (odpowiedz):
